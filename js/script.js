a85b9187c0c5160f86d2cea39ff2d2f1bb91af44
PedroDeVvV <pedrohgs2004@gmail.com>
2022-08-28 14:50:58 -0300

    Páginas de exclusivos de cada marca

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log('Hello World!'); 
+/*
+Comentarios de bloco
+*/ 
+
+let n1 = 5; //Local var
+const n2 = 10; //Constant
+var n3 = 2; //Global
+let total = n1 + n2 + n3;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log('A soma desses três números é igual á ' + total);
+
+
+
+alert('Seja bem vindo, o resultado desta soma é igual á ' + total);
+alert('Este é um site sobre 8 exclusivos da Microsoft')
+console.log(`O valor da soma é: ${total}`)
